a9f5a2bf916727a465cd5f946568d420843d0f6f
Noor696 <eng.nooralhateeb@gmail.com>
2023-01-13 16:12:42 +0000

    tree chalenge 01

--- python/code_challenges/tree/challenge01/test_challenge01.py
+++ python/code_challenges/tree/challenge01/test_challenge01.py
@@ -1,1 +1,27 @@
 # Write your test here
+
+from challenge01 import Node, Tree , buildTree
+
+
+tree1 = Tree()
+
+def test_tree1():
+    
+    
+    actual = buildTree(tree1.binaryTree([3,9,20,15,7],[9,3,15,20,7]))
+    expect = [3, 9, 20, 'null', 'null', 15, 7]
+
+    assert actual == expect
+    # assert buildTree(tree1.binaryTree([3,9,20,15,7],[9,3,15,20,7])) == [3, 9, 20, 'null', 'null', 15, 7]
+
+def test_tree2():
+    
+    
+    actual = buildTree(tree1.binaryTree([4,3,6],[3,4,6]))
+    expect = [4, 3, 6, 'null', 'null']
+
+    assert actual == expect
+
+
+
+
